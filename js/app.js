a359d713809a498f57250a6259463093b7d902de
Razi <razibhatti1@gmail.com>
2019-09-24 00:17:05 +0500

    initial commit

---
+++ js/app.js
@@ -0,0 +1,33 @@
+jQuery(document).ready(function($) {
+  $("#coin").on("click", function() {
+    var flipResult = Math.random();
+    $("#coin").removeClass();
+    setTimeout(function() {
+      if (flipResult <= 0.5) {
+        $("#coin").addClass("heads");
+        setTimeout(() => {
+          document.getElementById("result").innerHTML = "Chaand";
+          $("#exampleModalLong").modal("show");
+        }, 2800);
+      } else {
+        $("#coin").addClass("tails");
+        setTimeout(() => {
+          document.getElementById("result").innerHTML = "Paanch";
+          $("#exampleModalLong").modal("show");
+        }, 2800);
+      }
+    }, 100);
+  });
+});
+
+function enableToss(){
+  console.log("check");
+  document.getElementById("coin").style.pointerEvents = "auto";
+  document.getElementById("coin").style.border ="4px solid rgb(77, 240, 86,0.5)";
+  document.getElementById("coin").style.borderRadius = "50%";
+};
+
+function loadGame() {
+  document.getElementsByClassName("load")[0].style.display = "none";
+  document.getElementsByClassName("gameContainer")[0].style.display = "block";
+}
