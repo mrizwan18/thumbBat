ecba7e9285373c3b0ff70300d4e314bafcd9f6df
Razi <razibhatti1@gmail.com>
2019-09-28 14:00:39 +0500

    Added innings selction

--- js/app.js
+++ js/app.js
@@ -1,18 +1,42 @@
+let toss_val;
+let player;
+
+
 jQuery(document).ready(function ($) {
   $("#coin").on("click", function () {
-    var flipResult = Math.random();
+    if (document.getElementById("paanch").checked) {
+      toss_val = "Paanch";
+    }
+    else
+      toss_val = "Chaand";
+    var flipResult = Math.random(0,1);
+    console.log(flipResult);
     $("#coin").removeClass();
     setTimeout(function () {
       if (flipResult <= 0.5) {
         $("#coin").addClass("heads");
         setTimeout(() => {
-          document.getElementById("result").innerHTML = "Chaand";
+          if (toss_val === "Chaand") {
+            document.getElementsByClassName("selectInnings")[0].style.display = "block";
+            document.getElementById("result").innerHTML = "You Won the toss";
+          }
+          else {
+            document.getElementsByClassName("selectInnings")[0].style.display = "none";
+            document.getElementById("result").innerHTML = "You Lost the toss";
+          }
           $("#exampleModalLong").modal("show");
         }, 2800);
       } else {
         $("#coin").addClass("tails");
         setTimeout(() => {
-          document.getElementById("result").innerHTML = "Paanch";
+          if (toss_val === "Paanch") {
+            document.getElementsByClassName("selectInnings")[0].style.display = "block";
+            document.getElementById("result").innerHTML = "You Won the toss";
+          }
+          else {
+            document.getElementsByClassName("selectInnings")[0].style.display = "none";
+            document.getElementById("result").innerHTML = "You Lost the toss";
+          }
           $("#exampleModalLong").modal("show");
         }, 2800);
       }
@@ -25,12 +49,9 @@ function enableToss() {
   let old = document.getElementById("coin").style.animation;
   document.getElementById("coin").style.animation = "scale-up-center 0.4s cubic-bezier(0.390, 0.575, 0.565, 1.000) both";
   setTimeout(() => {
-    document.getElementById("coin").style.animation=old;
+    document.getElementById("coin").style.animation = old;
   }, 1000);
 };
-
-purify(content, css, options)
-
 function loadGame() {
   document.getElementsByClassName("load")[0].style.display = "none";
   document.getElementsByClassName("toss")[0].style.display = "block";
