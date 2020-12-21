aeb630c87f381ff12977d4b4c23ea69669fb5d6a
Muhammad Rizwan <mrizwan02@i2c.com>
2020-12-21 18:25:39 +0500

    fixed some modal issues

--- app.js
+++ app.js
@@ -30,11 +30,12 @@ app.use((req, res, next) => {
 });
 
 app.use((error, req, res, next) => {
-    res.status(error.status || 500).json({
-        error: {
-            message: error.message
-        }
-    });
+    res.render("error", {errorMessage: error.message})
+    // res.status(error.status || 500).json({
+    //     error: {
+    //         message: error.message
+    //     }
+    // });
 });
 
 module.exports = app;
