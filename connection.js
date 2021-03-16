c2394b1e14bb14d2ea778796570adcbb45ee909d
Muhammad Rizwan <mrizwan02@i2c.com>
2021-03-16 12:48:58 +0500

    deploying changes

--- connection.js
+++ connection.js
@@ -15,8 +15,7 @@ function isUserPresent(username, res) {
 
 exports.addUser = function addUser(username, res) {
   isUserPresent(username, function (result) {
-    console.log(result)
-    if (result) {
+    if (!result) {
       const newUser = new User({
         username
       });
